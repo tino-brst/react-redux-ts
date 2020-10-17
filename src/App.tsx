72cc802a91acb0eb14e72870fac57e77ed8f311c
Agustin Burset <agustin.burset@icloud.com>
2020-10-17 14:42:09 -0300

    src Prettier pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 interface AppProps {}
 
 function App({}: AppProps) {
   // Create the count state.
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   // Create the counter (+1 every second).
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
+    const timer = setTimeout(() => setCount(count + 1), 1000)
+    return () => clearTimeout(timer)
+  }, [count, setCount])
   // Return the App component.
   return (
     <div className="App">
@@ -35,7 +35,7 @@ function App({}: AppProps) {
         </p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
