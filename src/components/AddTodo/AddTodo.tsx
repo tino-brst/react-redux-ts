e4af8c1c31f28fd977d810128beca341f4bfd72e
Agustin Burset <agustin.burset@icloud.com>
2020-10-18 11:59:26 -0300

    Connect AddTodo component to store

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,10 +1,55 @@
 import React from 'react'
+import { bindActionCreators, Dispatch } from 'redux'
+import { connect } from 'react-redux'
+import type { StoreState } from 'app/store'
+import { addTodo } from 'app/store/todos/actions'
 
-export function AddTodo() {
+type DispatchProps = {
+  addTodo: (title: string) => void
+}
+
+type Props = DispatchProps
+
+function AddTodo(props: Props) {
+  const [inputValue, setInputValue] = React.useState('')
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.target.value)
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    props.addTodo(inputValue)
+    setInputValue('')
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="new todo" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="new todo"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
       <input type="submit" value="Add" />
     </form>
   )
 }
+function mapState(state: StoreState) {
+  return {
+    todos: state.todos,
+  }
+}
+
+function mapDispatch(dispatch: Dispatch) {
+  return bindActionCreators(
+    {
+      addTodo,
+    },
+    dispatch,
+  )
+}
+
+const ConnectedAddTodo = connect(mapState, mapDispatch)(AddTodo)
+
+export { ConnectedAddTodo as AddTodo }
