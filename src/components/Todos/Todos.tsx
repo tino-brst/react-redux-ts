0914c3e90d03d15a320ce236ed1f5461a29cd531
Agustin Burset <agustin.burset@icloud.com>
2020-10-18 11:40:58 -0300

    Connect Todos component to store

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,16 +1,58 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
+import type { StoreState } from 'app/store'
+import type { Todo } from 'app/types'
+import { toggleTodo } from 'app/store/todos/actions'
 
-export function Todos() {
+type StateProps = {
+  todos: Array<Todo>
+}
+
+type DispatchProps = {
+  toggleTodo: (id: string) => void
+}
+
+type OwnProps = {}
+
+type Props = StateProps & DispatchProps & OwnProps
+
+function Todos(props: Props) {
   return (
     <div>
       <ul>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Find Luke
-          </label>
-        </li>
+        {props.todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => props.toggleTodo(todo.id)}
+              />
+              {todo.title}
+            </label>
+          </li>
+        ))}
       </ul>
     </div>
   )
 }
+
+function mapState(state: StoreState) {
+  return {
+    todos: state.todos,
+  }
+}
+
+function mapDispatch(dispatch: Dispatch) {
+  return bindActionCreators(
+    {
+      toggleTodo,
+    },
+    dispatch,
+  )
+}
+
+const ConnectedTodos = connect(mapState, mapDispatch)(Todos)
+
+export { ConnectedTodos as Todos }
