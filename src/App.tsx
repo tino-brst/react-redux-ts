e1359bfd30d406230af53b0aad5ae8772d02e3c3
Agustin Burset <agustin.burset@icloud.com>
2020-10-19 07:35:32 -0300

    Load todos on app start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { bindActionCreators, Dispatch } from 'redux'
+import { connect } from 'react-redux'
 import { AddTodo, FilterSelect, Todos } from 'app/components'
+import { loadTodos } from 'app/store/todos/actions'
 
-export function App() {
+type DispatchProps = {
+  loadTodos: () => void
+}
+
+type Props = DispatchProps
+
+function App(props: Props) {
+  React.useEffect(() => {
+    props.loadTodos()
+  }, [])
+
   return (
     <div className="app">
       <h1>Todos</h1>
@@ -11,3 +24,16 @@ export function App() {
     </div>
   )
 }
+
+function mapDispatch(dispatch: Dispatch) {
+  return bindActionCreators(
+    {
+      loadTodos,
+    },
+    dispatch,
+  )
+}
+
+const ConnectedApp = connect(null, mapDispatch)(App)
+
+export { ConnectedApp as App }
