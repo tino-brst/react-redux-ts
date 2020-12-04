1065456cfaef76b83700209fdd74be55c067cc28
Agustin Burset <agustin.burset@icloud.com>
2020-12-04 10:29:59 -0300

    Update store to use new reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { Action, applyMiddleware, combineReducers, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk, { ThunkAction } from 'redux-thunk'
-import { todosReducer } from './todos/reducer'
-import { activeFilterReducer } from './activeFilter/reducer'
-import type { TodosAction } from './todos/types'
-import type { ActiveFilterAction } from './activeFilter/types'
+import { todosReducer } from './todos'
+import { activeFilterReducer } from './activeFilter'
 
 const rootReducer = combineReducers({
   todos: todosReducer,
@@ -17,5 +15,5 @@ const storeEnhancer = composeWithDevTools(applyMiddleware(...middleware))
 export const store = createStore(rootReducer, storeEnhancer)
 
 export type AppState = ReturnType<typeof rootReducer>
-export type AppAction = TodosAction | ActiveFilterAction
+export type AppAction = Action<string>
 export type AppThunkAction = ThunkAction<void, AppState, unknown, AppAction>
